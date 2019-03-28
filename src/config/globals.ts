5402732e8f703bec2e993037739d5b77610a61ce
Beau Smith <beau@beausmith.com>
2019-03-27 20:03:14 -0700

    VVSG 2.0 Font Sizes (WIP)

--- src/config/globals.ts
+++ src/config/globals.ts
@@ -1,4 +1,15 @@
 export default {
-  FONT_SIZES: [18, 24, 28, 32],
+  FONT_SIZES: [18, 24, 44, 50], // VVSG 2.0 sizes
   TEXT_SIZE: 1,
 }
+
+// TODO: VVSG 2.0 Font Sizes
+// 7.1 - G – Text size(electronic display)
+// A voting system’s electronic display must be capable of showing all information in a range of text sizes that voters can select from, with a  default text size at least 4.8 mm(based on the height of the uppercase I), allowing voters to both increase and decrease the text size.
+// The voting system may meet this requirement in one of the following ways:
+// 1.    Provide continuous scaling with a minimum increment of .5 mm that covers the full range of text sizes from 3.5 mm to 9.0 mm.
+// 2.    Provide at least four discrete text sizes, in which the main ballot options fall within one of these ranges.
+//   a) 3.5 - 4.2 mm(10 - 12 points)
+// b) 4.8 - 5.6 mm(14 - 16 points)
+// c) 6.4 - 7.1 mm(18 - 20 points)
+// d) 8.5 - 9.0 mm(24 - 25 points)
