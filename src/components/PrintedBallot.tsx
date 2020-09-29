0b6c4b7d7e65567066b1c4f1a9626465147010d0
Ben Adida <ben@adida.net>
2020-09-29 14:14:36 -0700

    format date objects

--- src/components/PrintedBallot.tsx
+++ src/components/PrintedBallot.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+
 import {
   encodeBallot,
   BallotType,
@@ -29,6 +30,8 @@ import {
 import QRCode from './QRCode'
 import Prose from './Prose'
 import Text, { NoWrap } from './Text'
+
+import { dateLong } from '../utils/date'
 
 const Ballot = styled.div`
   page-break-after: always;
@@ -224,7 +227,7 @@ const PrintBallot = ({
             {partyPrimaryAdjective} {title}
           </h3>
           <p>
-            {date}
+            {dateLong(date)}
             <br />
             {county.name}, {state}
           </p>
